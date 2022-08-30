54c4e9b96cd517645ad19c7802a0b63c2dc27f83
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-08-30 19:03:16 +0200

    chore: add logging for development environment (#8)

--- src/server/database/client.ts
+++ src/server/database/client.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { env } from "@/environment/server.mjs";
 
 declare global {
-  // eslint-disable-next-line no-var
+  /* eslint-disable-next-line no-var */
   var prisma: PrismaClient | undefined;
 }
 
@@ -13,7 +13,8 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query"],
+    log:
+      env.NODE_ENV === "development" ? ["error", "query", "warn"] : ["error"],
   });
 
 if (env.NODE_ENV !== "production") {
--- src/server/trpc/with-trpc.ts
+++ src/server/trpc/with-trpc.ts
@@ -1,3 +1,5 @@
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import { loggerLink } from "@trpc/client/links/loggerLink";
 import { withTRPC as setupTRPC } from "@trpc/next";
 import superjson from "superjson";
 
@@ -7,6 +9,16 @@ import type { AppRouter } from "@/server/router";
 
 export const withTRPC = setupTRPC<AppRouter>({
   config: () => ({
+    links: [
+      loggerLink({
+        enabled: (options) =>
+          process.env.NODE_ENV === "development" ||
+          (options.direction === "down" && options.result instanceof Error),
+      }),
+      httpBatchLink({
+        url: `${env.NEXT_PUBLIC_BASE_URL}/api/trpc`,
+      }),
+    ],
     /**
      * TODO: Choose a sane `staleTime` for React Query.
      *
