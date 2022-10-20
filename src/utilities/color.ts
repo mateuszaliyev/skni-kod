f12ef38bf1ba841b40b636bd1bc79315e6f359b1
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-10-21 00:19:00 +0200

    feat: add open graph image generation for posts

---
+++ src/utilities/color.ts
@@ -0,0 +1,25 @@
+export const colorFromText = (text: string) => {
+  let hash = 0;
+  let i;
+
+  for (i = 0; i < text.length; i++) {
+    hash = text.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+
+  return color;
+};
+
+export const getHexColorBrightness = (color: string) => {
+  const r = parseInt(color.slice(1, 3), 16);
+  const g = parseInt(color.slice(3, 5), 16);
+  const b = parseInt(color.slice(5, 7), 16);
+
+  return (r + g + b) / 3;
+};
