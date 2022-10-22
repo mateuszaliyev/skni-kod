19de1b9b610b0f3d005ec502a9b52ef1bb64ab2a
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-10-22 21:52:34 +0200

    build: update dependencies (#35)

--- src/i18n/is-locale.ts
+++
@@ -1,5 +0,0 @@
-import type { Locale } from "./types";
-import { locales } from "./utilities";
-
-export const isLocale = (locale: string): locale is Locale =>
-  locales.includes(locale as Locale);
