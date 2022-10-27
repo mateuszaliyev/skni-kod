67c7272ac28bc9e407f9c81951a5a4c220bf5108
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-10-28 00:14:29 +0200

    feat: display user's avatar when signed in

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,6 +1,8 @@
 import { type HTMLAttributes, useEffect, useState } from "react";
 import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import { clsx } from "clsx";
 
 import { ButtonMenu } from "@/components/button";
@@ -24,6 +26,8 @@ export const Navigation = ({
   ...props
 }: NavigationProps) => {
   const { LL } = useI18nContext();
+
+  const session = useSession();
 
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -51,13 +55,35 @@ export const Navigation = ({
       <nav
         className={clsx(
           isLargeScreen
-            ? "ml-auto space-x-6"
+            ? "ml-auto flex items-center gap-6"
             : "fixed inset-0 z-navigation flex-col divide-y divide-black/10 bg-white/80 px-6 pt-20 backdrop-blur-sm backdrop-saturate-[180%] transition-[opacity,transform] duration-500 dark:divide-white/10 dark:bg-black/50",
           !isLargeScreen && menuOpen && "flex",
           !isLargeScreen && !menuOpen && "hidden"
         )}
       >
         {children}
+        {session.status === "unauthenticated" && (
+          <button
+            className="flex h-10 items-center justify-center rounded-md border border-black bg-black px-3 text-sm font-bold text-white transition-colors hover:bg-transparent hover:text-current dark:border-white dark:bg-white dark:text-black dark:hover:bg-transparent dark:hover:text-current lg:h-8"
+            onClick={() => void signIn("github")}
+          >
+            {LL.signIn()}
+          </button>
+        )}
+        {isLargeScreen &&
+          session.status === "authenticated" &&
+          session.data.user?.image &&
+          session.data.user?.name && (
+            <button onClick={() => void signOut()}>
+              <img
+                alt={session.data.user.name}
+                className="cursor-pointer rounded-full"
+                height={56}
+                src={session.data.user.image}
+                width={56}
+              />
+            </button>
+          )}
         {!isLargeScreen && (
           <div className="flex justify-center space-x-4">
             <Link href={SKNI_KOD_FACEBOOK} target="_blank">
