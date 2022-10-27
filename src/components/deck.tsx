d376f5d1fa6c41ac0a76d39cfed8fe9cd0ed21fb
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-10-27 21:10:02 +0200

    refactor: set `Deck` text color to `gray-500`

--- src/components/deck.tsx
+++ src/components/deck.tsx
@@ -8,7 +8,7 @@ export const Deck = forwardRef(
   ({ as: Component = "p", className, ...props }, ref) => (
     <Component
       className={clsx(
-        "mx-auto max-w-prose text-center text-xl text-gray-500 dark:text-gray-400",
+        "mx-auto max-w-prose text-center text-xl text-gray-500",
         className
       )}
       ref={ref}
