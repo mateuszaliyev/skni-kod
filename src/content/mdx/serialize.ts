bdcccca33ebc06e24c8d4c775e24f9f5b9a213d3
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2023-02-18 21:01:37 +0100

    fix: make shiki available at runtime

--- src/content/mdx/serialize.ts
+++ src/content/mdx/serialize.ts
@@ -5,53 +5,23 @@ import {
   type Options as RehypeAutolinkHeadingsOptions,
 } from "rehype-autolink-headings";
 import rehypeKatex from "rehype-katex";
-import {
-  default as rehypePrettyCode,
-  type Options as RehypePrettyCodeOptions,
-} from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 
-import dark from "./themes/dark.json";
-import light from "./themes/light.json";
-
-type RehypePrettyCodeNode = {
-  children: unknown[];
-  properties: {
-    className: string[];
-  };
-};
+import { rehypeCode } from "./code";
 
 const rehypeAutolinkHeadingsOptions: RehypeAutolinkHeadingsOptions = {
   behavior: "wrap",
 };
 
-const rehypePrettyCodeOptions: Partial<RehypePrettyCodeOptions> = {
-  onVisitHighlightedLine: (node: RehypePrettyCodeNode) => {
-    node.properties.className.push("highlighted");
-  },
-  onVisitHighlightedWord: (node: RehypePrettyCodeNode) => {
-    node.properties.className = ["word"];
-  },
-  onVisitLine: (node: RehypePrettyCodeNode) => {
-    if (node.children.length === 0) {
-      node.children = [{ type: "text", value: " " }];
-    }
-  },
-  theme: {
-    dark,
-    light,
-  } as unknown as Record<string, JSON>,
-};
-
-export const serializeMdx = async (source: string) =>
-  await serialize(source, {
+export const serializeMdx = (source: string) =>
+  serialize(source, {
     mdxOptions: {
       format: "mdx",
       rehypePlugins: [
         rehypeKatex,
-        [rehypePrettyCode, rehypePrettyCodeOptions],
+        rehypeCode,
         rehypeSlug,
         [rehypeAutolinkHeadings, rehypeAutolinkHeadingsOptions],
       ],
